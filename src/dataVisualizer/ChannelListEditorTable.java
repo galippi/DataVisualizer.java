87f5ad2af910f4955f1c7ff0da698df7fe095348
Gabor Liptak <liptakgabor@freemail.hu>
2021-09-25 17:46:20 +0200

    ChannelListEditorTable: simplification

--- src/dataVisualizer/ChannelListEditorTable.java
+++ src/dataVisualizer/ChannelListEditorTable.java
@@ -4,63 +4,26 @@ import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
 //javax.swing.table.DefaultTableModel
 
-class MyTableModel extends AbstractTableModel {
+class MyTableModel extends javax.swing.table.DefaultTableModel {
     private String[] columnNames = {"Signal name", "Signal color", "Group name"};
-    private Object[][] data = {{"Signal 0", "red", "group 0"}};
-    //private Object[][] data = null;
+    private String[][] data = {{"Signal 0", "red", "group 0"}};
 
     public MyTableModel()
     {
         //super(data, columnNames);
         //super(columnNames);
         super();
-        //for (int i = 0; i < columnNames.length; i++)
-        //    addColumn(columnNames[i]);
-        //addRow(data);
-    }
-
-    public int getColumnCount() {
-        return columnNames.length;
-    }
-
-    public int getRowCount() {
-        return data.length;
-        //return 1;
-    }
-
-    public String getColumnName(int col) {
-        return columnNames[col];
-    }
-
-    public Object getValueAt(int row, int col) {
-        return data[row][col];
-    }
-
-    public Class getColumnClass(int c) {
-        return getValueAt(0, c).getClass();
-    }
-
-    /*
-     * Don't need to implement this method unless your table's
-     * editable.
-     */
-    public boolean isCellEditable(int row, int col) {
-        //Note that the data/cell address is constant,
-        //no matter where the cell appears onscreen.
-        if (col < 2) {
-            return false;
-        } else {
-            return true;
+        for (int i = 0; i < columnNames.length; i++)
+        {
+            addColumn(columnNames[i]);
         }
-    }
 
-    /*
-     * Don't need to implement this method unless your table's
-     * data can change.
-     */
-    public void setValueAt(Object value, int row, int col) {
-        data[row][col] = value;
-        fireTableCellUpdated(row, col);
+        setNumRows(1);
+        //addRow(data);
+        for (int i=0; i < columnNames.length; i++)
+        {
+            setValueAt(data[0][i], 0, i);
+        }
     }
 
     /**
