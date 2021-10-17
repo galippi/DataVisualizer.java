511f2fc2a9d14dcf20a2acd3d8d216b0ba84ec14
Gabor Liptak <liptakgabor@freemail.hu>
2021-10-17 08:55:23 +0200

    DataChannelGroup: signal offset/factor is added

--- src/dataVisualizer/DataChannelGroup.java
+++ src/dataVisualizer/DataChannelGroup.java
@@ -4,6 +4,10 @@ public class DataChannelGroup {
     DataChannelGroup(String _name)
     {
         name = _name;
+        offset = 0;
+        factor = 1.0;
     }
     String name;
+    double offset;
+    double factor;
 }
