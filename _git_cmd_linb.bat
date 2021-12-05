097f33b74f5907b28de0e99f6999103092ee1c81
Gabor Liptak <liptakgabor@freemail.hu>
2021-12-05 12:10:22 +0100

    git_cmd scripts are improved: parameter handling

--- _git_cmd_linb.bat
+++ _git_cmd_linb.bat
@@ -1,4 +1,13 @@
+@echo off
 set path=C:\Programok\git\bin;%PATH%
 set HOME=D:\Gabor
 ::set HOME=C:\Dokumentumok\Gabor\cygwin
+
+if "%*" == "" goto noparam
+%*
+goto end
+
+:noparam
 start "%CD%" cmd
+
+:end
